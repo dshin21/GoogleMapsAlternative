6d5cd6520a79ff56fa94ccc5a0582a95d82f4812
Daniel Shin <danielshin234@gmail.com>
2018-10-31 18:24:33 -0700

    finished

--- app/src/main/java/ca/bcit/dshin21/googlemapsalternative/MainActivity.java
+++ app/src/main/java/ca/bcit/dshin21/googlemapsalternative/MainActivity.java
@@ -3,11 +3,60 @@ package ca.bcit.dshin21.googlemapsalternative;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiZGFuaWVsc2hpbjIzNCIsImEiOiJjam54d2Y3ZXIwaWM3M3hxcGNkY3NobmhhIn0.qLO4pPm2mro3PicyoWrooA");
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
